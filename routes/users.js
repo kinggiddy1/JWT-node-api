049df0d36d8cec48971b9c72790289711893b7be
unknown <kinggiddy@gmail.com>
2025-03-16 21:18:26 +0200

    Added middleware

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const { register, login } = require('../controllers/userController');
+const auth = require('../middleware/authGuard');
+const authGuard = require('../middleware/authGuard');
 
 
   //Register
-  router.post('/register', register)
+  router.post('/register', authGuard ,register)
     //Login
   router.post('/login', login)
     
-  
-
-
 
 module.exports = router;
