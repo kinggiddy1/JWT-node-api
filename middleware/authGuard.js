9582a8925a1fa8b5c19fdbdf6d6eede7f03d41b9
unknown <kinggiddy@gmail.com>
2025-03-16 21:38:24 +0200

    Added middleware

--- middleware/authGuard.js
+++ middleware/authGuard.js
@@ -4,15 +4,12 @@ require('dotenv').config();
 module.exports = function(req, res, next) {
   const authHeader = req.header('Authorization');
   
-  // Check if Authorization header exists and has correct format
   if (!authHeader || !authHeader.startsWith('Bearer ')) {
     return res.status(401).json({ error: 'Authorization denied. No token found.' });
   }
   
-  // Extract the token
   const token = authHeader.substring(7);
   
-  // Verify token
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded.user;
