634063bb65515e9654cac34edeb0de9fd0982680
unknown <kinggiddy@gmail.com>
2025-03-18 19:55:42 +0200

    Added CORS

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 const express = require('express');
 require('dotenv').config();
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+
+
+
+const PORT = process.env.PORT || 3000;
 
 // CORS
 app.use(cors({
     origin: 'http://localhost:4200',
     credentials: true
   }));
-
-const PORT = process.env.PORT || 3000;
-
+  
 // Basic route
 app.get('/', (req, res) => {
     res.json({ message: 'This API is working smoothly' });
